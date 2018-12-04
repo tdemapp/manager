b23d7d5d2d87318a2aebf37da36bb600faeabfd2
N U R O ™ <bendixon50@gmail.com>
2018-12-04 10:47:16 +0000

    :art:

--- src/options/options.vue
+++ src/options/options.vue
@@ -6,7 +6,7 @@
 			<v-container fluid>
                 <v-layout row wrap>
                     <v-flex xs12 sm12 md6 lg3 xl3 v-for='(item, i) in extensionsStorage' :key='i'>
-						<CardExtension :name='item.name' :version='item.version' :description='item.description' :isEnabled='item.enabled' />
+						<Extension :name='item.name' :version='item.version' :description='item.description' :isEnabled='item.enabled' />
                     </v-flex>
                 </v-layout>
             </v-container>
@@ -15,13 +15,13 @@
 </template>
 
 <script>
-import * as util from '../js/util';
-import CardExtension from './components/cardExtension';
+import * as util from '~/js/util';
+import Extension from './components/extension';
 import Toolbar from './components/toolbar.vue';
 
 export default {
 	components: {
-		CardExtension,
+		Extension,
 		Toolbar
 	},
 	data() {
