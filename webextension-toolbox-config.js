bb06115f07354c6fdce11f1c033b04f1926e0c29
[MEADOW_DEV] <bendixon50@gmail.com>
2018-12-27 23:41:12 +0000

    :rotating_light:

--- webextension-toolbox-config.js
+++ webextension-toolbox-config.js
@@ -2,31 +2,33 @@ const webpack = require('webpack');
 const { VueLoaderPlugin } = require('vue-loader');
 
 module.exports = {
-  webpack: (config, { dev, vendor }) => {
-    // Add Vue to file extensions
-    config.resolve.extensions = ['.js', '.json', '.vue'];
+	webpack: (config, { dev, vendor }) => {
+		// Add Vue to file extensions
+		config.resolve.extensions = ['.js', '.json', '.vue'];
 
-    // Set entry points
-    config.entry = {
-      'js/content': './js/content.js',
-      'options/options': './options/options.js',
-    };
+		// Set entry points
+		config.entry = {
+			'js/content': './js/content.js',
+			'options/options': './options/options.js'
+		};
 
-    // Add Vue loader to module rules
-    config.module.rules.push({
-        test: /\.vue$/,
-        use: ['vue-loader'],
-    });
+		// Add Vue loader to module rules
+		config.module.rules.push({
+			test: /\.vue$/,
+			use: ['vue-loader'],
+		});
 
-    // Add CSS loader to module rules
-    config.module.rules.push({
-      test: /\.css$/,
-      use: ['style-loader', 'css-loader'],
-    });
+		// Add CSS loader to module rules
+		config.module.rules.push({
+			test: /\.css$/,
+			use: ['style-loader', 'css-loader']
+		});
 
-    // Create new Vue Loader plugin instance
-    config.plugins.push(new VueLoaderPlugin());
+		// Create new Vue Loader plugin instance
+		config.plugins.push(new VueLoaderPlugin());
 
-    return config
-  }
-}
+		console.log(config);
+
+		return config;
+	}
+};
