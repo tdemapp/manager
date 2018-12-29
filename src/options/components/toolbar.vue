53f597a13c4d53203e4d87642ee60241728bc411
N U R O ™ <bendixon50@gmail.com>
2018-12-29 01:55:59 +0000

    :recycle:

--- src/options/components/toolbar.vue
+++ src/options/components/toolbar.vue
@@ -14,9 +14,9 @@
 				<v-btn
 					flat
 					large
-					@click="dialogSettings = true"
+					@click="dialogs.settings = true"
 					v-shortkey="[',']"
-					@shortkey="dialogSettings = true"
+					@shortkey="dialogs.settings = true"
 					light
 					class="mt-3"
 				>
@@ -26,9 +26,9 @@
 				<v-btn
 					flat
 					large
-					@click="dialogInfo = true"
+					@click="dialogs.info = true"
 					v-shortkey="['i']"
-					@shortkey="dialogInfo = true"
+					@shortkey="dialogs.info = true"
 					light
 					class="mt-3"
 				>
@@ -38,7 +38,7 @@
 			</div>
 		</v-toolbar>
 
-		<v-dialog v-model="dialogSettings" max-width="640">
+		<v-dialog v-model="dialogs.settings" max-width="640">
 			<v-card>
 				<v-toolbar flat dark color="grey darken-4">
 					<v-icon v-html="$vuetify.icons.settings" />
@@ -48,7 +48,7 @@
 					/>
 					<v-spacer />
 					<v-toolbar-items class="hidden-sm-and-down">
-						<v-btn flat class="squareCorners" @click="dialogSettings = false">
+						<v-btn flat class="squareCorners" @click="dialogs.settings = false">
 							<v-icon v-html="$vuetify.icons.x" />
 						</v-btn>
 					</v-toolbar-items>
@@ -80,137 +80,45 @@
 			</v-card>
 		</v-dialog>
 
-		<v-dialog v-model="dialogInfo" max-width="640">
+		<v-dialog v-model="dialogs.info" max-width="840">
 			<v-card>
 				<v-toolbar flat dark color="grey darken-4">
 					<v-icon v-html="$vuetify.icons.info" />
-					<v-card-title
-						class="headline spacedLetters upperCase ml-2"
-						v-html="getLocale('dashboard_dialog_info_title')"
-					/>
+					<v-card-title class="headline spacedLetters upperCase ml-2" v-html="getLocale('dashboard_dialog_info_title')" />
 					<v-spacer />
 					<v-toolbar-items class="hidden-sm-and-down">
-						<v-btn flat class="squareCorners" @click="dialogInfo = false">
+						<v-btn flat class="squareCorners" @click="dialogs.info = false">
 							<v-icon v-html="$vuetify.icons.x" />
 						</v-btn>
 					</v-toolbar-items>
 				</v-toolbar>
 
 				<v-card-text>
-					<h1
-						class="headline spacedLetters upperCase boldTitle ma-2"
-						v-html="getLocale('dashboard_dialog_info_subtitle_links') + ':'"
-					/>
+					<!-- Links section -->
+					<h1 class="headline spacedLetters upperCase boldTitle ma-2" v-html="getLocale('dashboard_dialog_info_subtitle_links') + ':'" />
 					<v-divider />
-
 					<v-layout row wrap class="mt-2">
-						<!-- <v-flex xs5 class='mb-2 ml-4 mr-4'>
-							<v-btn flat large block target='_blank' href='https://chrome.google.com/webstore/detail/tdem/'>
-								<v-icon left v-html='$vuetify.icons.chrome' />
-								<span v-html='getLocale("dashboard_dialog_info_btn_chrome")' />
-							</v-btn>
-						</v-flex> -->
-						<v-flex xs5 class="mb-2 ml-4 mr-4">
-							<v-btn
-								flat
-								large
-								block
-								target="_blank"
-								href="https://github.com/nurodev/tdem"
-							>
-								<v-icon left v-html="$vuetify.icons.github" />
-								<span>GitHub</span>
-							</v-btn>
-						</v-flex>
-						<v-flex xs5 class="mb-2 ml-4 mr-4">
-							<v-btn
-								flat
-								large
-								block
-								target="_blank"
-								href="https://twitter.com/nurodev"
-							>
-								<v-icon left v-html="$vuetify.icons.twitter" />
-								<span
-									v-html="
-										'@' +
-											getLocale('dashboard_dialog_info_btn_twitter_personal')
-									"
-								/>
-							</v-btn>
-						</v-flex>
-						<!-- <v-flex xs5 class='mb-2 ml-4 mr-4'>
-							<v-btn flat large block target='_blank' href='https://twitter.com/tdem_app'>
-								<v-icon left v-html='$vuetify.icons.twitter' />
-								<span v-html='"@" + getLocale("dashboard_dialog_info_btn_twitter_tdem")' />
-							</v-btn>
-						</v-flex> -->
-					</v-layout>
-
-					<h1
-						class="headline spacedLetters upperCase boldTitle ma-2"
-						v-html="getLocale('dashboard_dialog_info_subtitle_thanks') + ' ❤️'"
-					/>
-					<v-divider />
-
-					<v-layout row wrap class="mt-2">
-						<v-flex xs5 class="mb-2 ml-4 mr-4">
-							<v-btn
-								flat
-								large
-								block
-								target="_blank"
-								href="https://github.com/NuroDev/TDEM/graphs/contributors"
-							>
-								<v-icon left v-html="$vuetify.icons.users" />
-								<span
-									v-html="getLocale('dashboard_dialog_info_btn_contributors')"
-								/>
-							</v-btn>
-						</v-flex>
-						<v-flex xs5 class="mb-2 ml-4 mr-4">
-							<v-btn
-								flat
-								large
-								block
-								target="_blank"
-								href="https://github.com/NuroDev/TDEM/network/dependencies"
-							>
-								<v-icon left v-html="$vuetify.icons.grid" />
-								<span
-									v-html="getLocale('dashboard_dialog_info_btn_dependencies')"
-								/>
+						<v-flex xs3 class="mb-1 ml-4 mr-4" v-for='(item, i) in links' :key='i'>
+							<v-btn flat large block target="_blank" :href='item.href' >
+								<v-icon left v-html='"$vuetify.icons." + item.icon' />
+								<span v-html='item.text'/>
 							</v-btn>
 						</v-flex>
 					</v-layout>
 
-					<h1
-						class="headline spacedLetters upperCase boldTitle ma-2"
-						v-html="getLocale('dashboard_dialog_info_subtitle_debug') + ':'"
-					/>
+					<!-- Debug section -->
+					<h1 class="headline spacedLetters upperCase boldTitle ma-2" v-html="getLocale('dashboard_dialog_info_subtitle_debug') + ':'" />
 					<v-divider />
-
 					<v-layout row wrap class="mt-2">
 						<v-flex xs5 class="ml-4 mr-4">
 							<v-btn flat large block disabled>
-								<span
-									class="spacedLetters"
-									v-html="
-										getLocale('dashboard_dialog_info_app_version') +
-											': ' +
-											extensionVersion
-									"
-								/>
+								<span class="spacedLetters" v-html="getLocale('dashboard_dialog_info_app_version') + ': ' + extensionVersion" />
 							</v-btn>
 						</v-flex>
 						<v-flex xs5 class="ml-4 mr-4">
 							<v-btn flat large block>
 								<v-icon left v-html="$vuetify.icons.download" />
-								<span
-									v-html="
-										getLocale('dashboard_dialog_info_btn_download_settings')
-									"
-								/>
+								<span v-html="getLocale('dashboard_dialog_info_btn_download_settings')" />
 							</v-btn>
 						</v-flex>
 					</v-layout>
@@ -230,9 +138,19 @@ export default {
 			extensionVersion: util.getExtensionVersion(),
 			infoTab: null,
 			searchInput: null,
-			dialogSearch: false,
-			dialogSettings: false,
-			dialogInfo: false,
+			dialogs: {
+				search: false,
+				settings: false,
+				info: false,
+			},
+			links: [
+				{ icon: 'twitter', text: util.getLocale('dashboard_dialog_info_btn_twitter_personal'), href: 'https://twitter.com/nurodev'},
+				{ icon: 'twitter', text: util.getLocale('dashboard_dialog_info_btn_twitter_tdem'), href: 'https://twitter.com/tdemapp'},
+				{ icon: 'chrome', text: util.getLocale('dashboard_dialog_info_btn_chrome'), href: 'https://chrome.google.com/webstore/detail/tdem/'},
+				{ icon: 'github', text: 'GitHub', href: 'https://github.com/nurodev/tdem'},
+				{ icon: 'users', text: util.getLocale('dashboard_dialog_info_btn_contributors'), href: 'https://github.com/NuroDev/TDEM/graphs/contributors'},
+				{ icon: 'grid', text: util.getLocale('dashboard_dialog_info_btn_dependencies'), href: 'https://github.com/NuroDev/TDEM/graphs/dependencies'},
+			]
 		};
 	},
 	methods: {
