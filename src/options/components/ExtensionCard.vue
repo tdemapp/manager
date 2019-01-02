2ae5a373acc599af5299c7bf08189d80a5d18cde
N U R O ™ <bendixon50@gmail.com>
2019-01-02 22:49:17 +0000

    :art: Improved extension component structure

--- src/options/components/ExtensionCard.vue
+++ src/options/components/ExtensionCard.vue
@@ -1,51 +1,51 @@
 <template>
 	<v-card class="ma-2 defaultCorners defaultShadow">
-		<v-toolbar flat :class="isEnabled ? 'grey darken-4 pt-3 pb-3' : 'white pt-3 pb-3'">
+		<v-toolbar flat :class="extension.isEnabled ? 'grey darken-4 pt-3 pb-3' : 'white pt-3 pb-3'">
 			<v-layout align-center justify-start row fill-height>
 				<v-flex xs1>
-					<v-icon large :dark="isEnabled" v-html="$vuetify.icons.box" />
+					<v-icon large :dark="extension.isEnabled" v-html="$vuetify.icons.box" />
 				</v-flex>
 				<v-flex xs9>
 					<v-card-text
-						:class="isEnabled ? 'white--text subheading' : 'grey--text subheading'"
+						:class="extension.isEnabled ? 'white--text subheading' : 'grey--text subheading'"
 					>
-						<span v-html="name" /> <br />
+						<span v-html="extension.name" /> <br />
 						<span
 							class="caption"
-							v-html="getLocale('dashboard_extension_version') + ': ' + version"
+							v-html="getLocale('dashboard_extension_version') + ': ' + extension.version"
 						/>
 					</v-card-text>
 				</v-flex>
 				<v-flex xs1>
 					<v-card-actions>
-						<v-switch class="mt-3" color="white" v-model="isEnabled" />
+						<v-switch class="mt-3" color="white" v-model="extension.isEnabled" />
 					</v-card-actions>
 				</v-flex>
 			</v-layout>
 		</v-toolbar>
 
-		<v-divider :class="isEnabled ? 'transparent ml-3 mr-3' : 'ml-3 mr-3'" />
+		<v-divider :class="extension.isEnabled ? 'transparent ml-3 mr-3' : 'ml-3 mr-3'" />
 
-		<v-card-text class="caption" v-html="description" />
+		<v-card-text class="caption" v-html="extension.description" />
 
 		<v-card-actions>
 			<v-btn
 				flat
 				icon
 				large
-				:color="isEnabled ? 'grey--darken-4' : 'grey'"
+				:color="extension.isEnabled ? 'grey--darken-4' : 'grey'"
 				target="_blank"
-				:href="website"
+				:href="extension.website"
 			>
 				<v-icon v-html="$vuetify.icons.link" />
 			</v-btn>
-			<v-btn flat icon large :color="isEnabled ? 'grey--darken-4' : 'grey'">
+			<v-btn flat icon large :color="extension.isEnabled ? 'grey--darken-4' : 'grey'">
 				<v-icon v-html="$vuetify.icons.refresh" />
 			</v-btn>
 
 			<v-spacer />
 
-			<v-btn flat icon large :color="isEnabled ? 'red' : 'grey'">
+			<v-btn flat icon large :color="extension.isEnabled ? 'red' : 'grey'">
 				<v-icon v-html="$vuetify.icons.trash" />
 			</v-btn>
 		</v-card-actions>
@@ -58,30 +58,10 @@ import extensionTemplate from '../../js/template';
 
 export default {
 	props: {
-		name: {
-			type: String,
+		extension: {
+			type: Object,
 			required: true,
-			default: extensionTemplate.name,
-		},
-		description: {
-			type: String,
-			required: true,
-			default: extensionTemplate.description,
-		},
-		version: {
-			type: String,
-			required: true,
-			default: extensionTemplate.version,
-		},
-		website: {
-			type: String,
-			required: false,
-			default: extensionTemplate.website,
-		},
-		isEnabled: {
-			type: Boolean,
-			required: true,
-			default: extensionTemplate.isEnabled,
+			default: extensionTemplate
 		},
 	},
 	methods: {
--- src/options/options.vue
+++ src/options/options.vue
@@ -6,12 +6,7 @@
 			<v-container fluid>
 				<v-layout row wrap>
 					<v-flex xs12 sm12 md6 lg3 xl3 v-for="(item, i) in extensionsStorage" :key="i">
-						<ExtensionCard
-							:name="item.name"
-							:version="item.version"
-							:description="item.description"
-							:isEnabled="item.enabled"
-						/>
+						<ExtensionCard :extension="item" />
 					</v-flex>
 				</v-layout>
 			</v-container>
@@ -21,11 +16,13 @@
 
 <script>
 import * as util from '../js/util';
+import AppNotification from './components/AppNotification.vue';
 import AppToolbar from './components/AppToolbar.vue';
 import ExtensionCard from './components/ExtensionCard.vue';
 
 export default {
 	components: {
+		AppNotification,
 		AppToolbar,
 		ExtensionCard,
 	},
