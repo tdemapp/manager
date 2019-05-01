e39ba3ba6e66bde39c81e3db55a249ba8ed5f44c
N U R O ™ <bendixon50@gmail.com>
2019-05-01 20:06:54 +0100

    :recycle: Moved to SFC for dialogs

---
+++ src/options/components/AppDialog.vue
@@ -0,0 +1,61 @@
+<template>
+	<section>
+		<!-- Sidebar Button -->
+		<v-list-tile ripple @click="dialog = true">
+			<v-list-tile-action> <slot name="icon" /> </v-list-tile-action>
+
+			<v-list-tile-content>
+				<v-list-tile-title class="spacedLetters text-uppercase" v-html="name" />
+			</v-list-tile-content>
+		</v-list-tile>
+
+		<!-- Dialog Content -->
+		<v-dialog v-model="dialog" :max-width="width">
+			<v-card>
+				<v-toolbar flat dark tabs color="grey darken-4">
+					<slot name="icon" />
+
+					<v-card-title
+						class="dialogTitle spacedLetters text-uppercase font-weight-thin ml-2"
+						v-html="name"
+					/>
+					<v-spacer />
+					<v-toolbar-items class="hidden-sm-and-down">
+						<v-btn flat class="squareCorners" @click="dialog = false">
+							<IconClose />
+						</v-btn>
+					</v-toolbar-items>
+				</v-toolbar>
+
+				<v-card-text> <slot name="content" /> </v-card-text>
+			</v-card>
+		</v-dialog>
+	</section>
+</template>
+
+<script>
+import IconClose from '../icons/x.svg';
+
+export default {
+	components: {
+		IconClose,
+	},
+	props: {
+		name: {
+			type: String,
+			default: 'Title',
+			required: true,
+		},
+		width: {
+			type: Number,
+			default: 440,
+			required: false,
+		},
+	},
+	data() {
+		return {
+			dialog: null,
+		};
+	},
+};
+</script>
--- src/options/components/AppSidebar.vue
+++ src/options/components/AppSidebar.vue
@@ -6,46 +6,209 @@
 		permanent
 		width="256"
 		v-model="drawer"
-		:class="$store.state.settings.isDarkTheme ? 'secondary' : 'primary'"
+		class="primary"
 		:mini-variant="$store.state.settings.isSidebarMini"
 	>
 		<v-list class="pt-0" three-line>
+			<!-- Sidebar toggle -->
 			<v-list-tile ripple @click="$store.commit('toggleSidebar')">
 				<v-list-tile-action> <IconMenu /> </v-list-tile-action>
 
 				<v-list-tile-content>
-					<v-list-tile-title class="spacedLetters" v-html="getLocale('dashboard_menu')" />
+					<v-list-tile-title
+						class="spacedLetters text-uppercase"
+						v-html="getLocale('dashboard_menu')"
+					/>
 				</v-list-tile-content>
 			</v-list-tile>
 
-			<DialogStore />
+			<!-- Store Dialog -->
+			<AppDialog :name="getLocale('dashboard_dialog_store_title')">
+				<IconStore slot="icon" />
 
-			<div style="position: fixed; bottom: 0; width: 256px;">
-				<DialogInfo />
-				<DialogSettings />
-			</div>
+				<v-layout align-center justify-center row wrap slot="content">
+					<span class="spacedLetters text-uppercase">Coming soon...</span>
+				</v-layout>
+			</AppDialog>
+
+			<!-- Info Dialog -->
+			<AppDialog :name="getLocale('dashboard_dialog_info_title')">
+				<IconInfo slot="icon" />
+
+				<v-layout row wrap slot="content">
+					<!-- Links -->
+					<v-flex xs12 v-for="(item, i) in infoLinks" :key="i">
+						<v-btn
+							flat
+							large
+							block
+							target="_blank"
+							:href="item.href"
+							class="spacedLetters"
+						>
+							<IconTwitter class="mr-3" />
+							<span v-html="item.text" />
+						</v-btn>
+					</v-flex>
+
+					<!-- Download Settings -->
+					<v-flex xs12>
+						<v-divider class="my-3" />
+
+						<v-btn
+							flat
+							large
+							block
+							href="`{data:text/json;charset=utf-8,${encodeURIComponent(JSON.stringify(this.props.objectToDownload))}}`"
+							download="settings.json"
+						>
+							<IconDownload class="mr-3" />
+							<span
+								class="spacedLetters"
+								v-html="getLocale('dashboard_dialog_info_download_settings')"
+							/>
+						</v-btn>
+					</v-flex>
+
+					<!-- App version -->
+					<v-flex xs12>
+						<v-btn flat large block disabled>
+							<span v-html="`v${extensionVersion}`" />
+						</v-btn>
+					</v-flex>
+				</v-layout>
+			</AppDialog>
+
+			<!-- Settings Dialog -->
+			<AppDialog :name="getLocale('dashboard_dialog_settings_title')" :width="740">
+				<IconSettings slot="icon" />
+
+				<div slot="content">
+					<h1
+						class="dialogTitle spacedLetters text-uppercase boldTitle ma-2 mb-3"
+						v-html="getLocale('dashboard_dialog_settings_subtitle_display') + ':'"
+					/>
+
+					<v-divider />
+
+					<v-layout align-center justify-center row wrap class="ml-4">
+						<v-flex xs4>
+							<v-switch
+								v-model="$store.state.settings.isDarkTheme"
+								class="mt- body-2 spacedLetters text-uppercase"
+								color="primary"
+								label="Dark Theme"
+							/>
+						</v-flex>
+						<v-flex xs4>
+							<v-switch
+								class="mt- body-2 spacedLetters text-uppercase"
+								color="primary"
+								label="Dark Theme"
+							/>
+						</v-flex>
+						<v-flex xs4>
+							<v-switch
+								class="mt- body-2 spacedLetters text-uppercase"
+								color="primary"
+								label="Dark Theme"
+							/>
+						</v-flex>
+					</v-layout>
+
+					<h1
+						class="dialogTitle spacedLetters text-uppercase boldTitle ma-2 my-3"
+						v-html="getLocale('dashboard_dialog_settings_subtitle_display') + ':'"
+					/>
+
+					<v-divider />
+
+					<v-layout align-center justify-center row wrap class="ml-4">
+						<v-flex xs4>
+							<v-switch
+								class="mt- body-2 spacedLetters text-uppercase"
+								color="primary"
+								label="Dark Theme"
+							/>
+						</v-flex>
+						<v-flex xs4>
+							<v-switch
+								class="mt- body-2 spacedLetters text-uppercase"
+								color="primary"
+								label="Dark Theme"
+							/>
+						</v-flex>
+						<v-flex xs4>
+							<v-switch
+								class="mt- body-2 spacedLetters text-uppercase"
+								color="primary"
+								label="Dark Theme"
+							/>
+						</v-flex>
+					</v-layout>
+				</div>
+			</AppDialog>
 		</v-list>
 	</v-navigation-drawer>
 </template>
 
 <script>
-import { getLocale, storage } from '../../scripts/util';
-import DialogInfo from './DialogInfo.vue';
-import DialogSettings from './DialogSettings.vue';
-import DialogStore from './DialogStore.vue';
+import { getExtensionVersion, getLocale, storage } from '../../scripts/util';
+
+import AppDialog from './AppDialog.vue';
+
+import IconClose from '../icons/x.svg';
+import IconContributors from '../icons/contributors.svg';
+import IconDependencies from '../icons/dependencies.svg';
+import IconDownload from '../icons/download.svg';
+import IconGithub from '../icons/github.svg';
+import IconInfo from '../icons/info.svg';
 import IconMenu from '../icons/menu.svg';
+import IconSettings from '../icons/settings.svg';
+import IconStore from '../icons/store.svg';
+import IconTwitter from '../icons/twitter.svg';
+import IconWebsite from '../icons/website.svg';
 
 export default {
 	components: {
-		DialogInfo,
-		DialogSettings,
-		DialogStore,
+		AppDialog,
+		IconClose,
+		IconContributors,
+		IconDependencies,
+		IconDownload,
+		IconGithub,
+		IconInfo,
 		IconMenu,
+		IconSettings,
+		IconStore,
+		IconTwitter,
+		IconWebsite,
 	},
 	data() {
 		return {
 			drawer: true,
+			extensionVersion: getExtensionVersion(),
+			settingsJSON: null,
+			infoLinks: [
+				{ text: 'Twitter', icon: `<IconTwitter />`, href: 'https://twitter.com/nurodev' },
+				{ text: 'GitHub', icon: `<IconGithub />`, href: 'https://github.com/nurodev' },
+				{ text: 'Website', icon: `<IconWebsite />`, href: 'https://tdem.app/' },
+				{
+					text: 'Contributors',
+					icon: `<IconContributors />`,
+					href: 'https://github.com/tdemapp/TDEM/graphs/contributors',
+				},
+				{
+					text: 'Dependencies',
+					icon: `<IconDependencies />`,
+					href: 'https://github.com/tdemapp/TDEM/network/dependencies',
+				},
+			],
 		};
+	},
+	created() {
+		this.$store.commit('getExtensionStorage');
+		this.settingsJSON = JSON.stringify(this.$store.state, null, 2);
 	},
 	methods: {
 		getLocale(text) {
