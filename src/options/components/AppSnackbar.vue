25150e00b43c24c3509efaf1fdefba1e8bf9cdf8
N U R O ™ <bendixon50@gmail.com>
2019-06-06 16:28:26 +0100

    :art: Snackbar itself is now a close button

--- src/options/components/AppSnackbar.vue
+++ src/options/components/AppSnackbar.vue
@@ -1,20 +1,23 @@
 <template>
-	<v-snackbar top right multi-line :color="color.background" v-model="active">
+	<v-snackbar
+		top
+		right
+		multi-line
+		class="snackbar"
+		:color="color.background"
+		v-model="active"
+		@click="active = false"
+	>
 		<IconDownload v-if="icon === 'download'" :class="`mr-4 ${color.icon}--text`" />
 		<IconError v-else-if="icon === 'error'" :class="`mr-4 ${color.icon}--text`" />
 		<IconSuccess v-else-if="icon === 'success'" :class="`mr-4 ${color.icon}--text`" />
 		<IconInfo v-else :class="`mr-4 ${color.icon}--text`" />
 
 		<span :class="`text-uppercase spacedLetters ${color.text}--text`" v-text="text" />
-
-		<v-btn flat large :color="color.icon" @click="active = false">
-			<IconClose />
-		</v-btn>
 	</v-snackbar>
 </template>
 
 <script>
-import IconClose from '../icons/x.svg';
 import IconDownload from '../icons/download.svg';
 import IconError from '../icons/error.svg';
 import IconInfo from '../icons/info.svg';
@@ -22,7 +25,6 @@ import IconSuccess from '../icons/success.svg';
 
 export default {
 	components: {
-		IconClose,
 		IconDownload,
 		IconError,
 		IconInfo,
