31b823aaeec330cd9b32f7c221da277a9d60a589
N U R O ™ <bendixon50@gmail.com>
2019-06-20 09:42:43 +0100

    :art: Cleaned up web extension toolbox config

--- webextension-toolbox-config.js
+++ webextension-toolbox-config.js
@@ -4,21 +4,24 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin');
 
 module.exports = {
-	webpack: (config, { dev, vendor }) => {
-		// Add Vue to file extensions
-		config.resolve.extensions = ['.js', '.json', '.vue'];
+	webpack: (config) => {
+		config.resolve.extensions = [
+			'.js',
+			'.json',
+			'.vue'
+		];
 
-		// Set custom entry points
 		config.entry = {
 			'scripts/content': './scripts/content.js',
 			'options/options': './options/options.js',
 		};
 
-		// Add custom module rules
 		config.module.rules.push(
 			{
 				test: /\.vue$/,
-				use: ['vue-loader'],
+				use: [
+					'vue-loader'
+				],
 			},
 			{
 				test: /\.css$/,
@@ -31,23 +34,36 @@ module.exports = {
 			},
 			{
 				test: /\.styl$/,
-				use: ['style-loader', 'css-loader', 'stylus-loader'],
+				use: [
+					'style-loader',
+					'css-loader',
+					'stylus-loader'
+				],
 			},
 			{
 				test: /\.svg$/,
 				issuer: /\.(vue|js|ts|svg)$/,
-				use: ['vue-loader', 'svg-to-vue-component/loader'],
+				use: [
+					'vue-loader',
+					'svg-to-vue-component/loader'
+				],
 			}
 		);
 
-		// Add custom plugins
-		config.plugins.push(new VueLoaderPlugin());
-		config.plugins.push(new MiniCssExtractPlugin());
-		config.plugins.push(new VuetifyLoaderPlugin());
+		config.plugins = [
+			...config.plugins,
+			new VueLoaderPlugin(),
+			new MiniCssExtractPlugin(),
+			new VuetifyLoaderPlugin(),
+		];
 
 		return config;
 	},
 
-	// Override default copyIgnore array
-	copyIgnore: ['**/*.js', '**/*.json', '**/*.css', '**/*.vue'],
+	copyIgnore: [
+		'**/*.js',
+		'**/*.json',
+		'**/*.css',
+		'**/*.vue'
+	],
 };
